e3262af21ee04f1bc57ba22bd1056c451abd69eb
MaltsevaNatalya94 <118058268+MaltsevaNatalya94@users.noreply.github.com>
2022-11-28 00:39:47 +0300

    Add files via upload

---
+++ Задача1.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите цикл, который принимает на вход два числа 
+(A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите степень");
+int deg = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= 1; i++)
+   Console.WriteLine($"Результат возведения в степень {(long)Math.Pow(num, deg)}");
+
+
+
+
+
+
+
+
